87800368f73ffbecea74e142eb7f8f3a3f9d02bf
Octavio Montt <octavio.montt@tails.com>
2025-08-26 09:50:54 +0100

    untill next time

--- main.go
+++ main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"slices"
+	"strings"
 	"sync/atomic"
+	"time"
+
+	"github.com/L-chaCon/go_server/internal/database"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+	db             *database.Queries
+}
+
 func main() {
-	apiCfg := apiConfig{fileserverHits: atomic.Int32{}}
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Printf("Unable to connect to database: %s", err)
+		return
+	}
+	dbQueries := database.New(db)
+
+	apiCfg := apiConfig{fileserverHits: atomic.Int32{}, db: dbQueries}
 	mux := http.NewServeMux()
 	mux.Handle(
 		"/app/",
@@ -18,19 +44,17 @@ func main() {
 			),
 		),
 	)
-	mux.HandleFunc("/healthz", healthzHandler)
-	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
-	mux.HandleFunc("/reset", apiCfg.resetHandler)
+	mux.HandleFunc("GET /api/healthz", healthzHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
+	mux.HandleFunc("POST /api/users", apiCfg.createUserHandler)
 
 	server := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
 	server.ListenAndServe()
-}
-
-type apiConfig struct {
-	fileserverHits atomic.Int32
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -41,19 +65,108 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(200)
-	response := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	response := fmt.Sprintf(`
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`,
+		cfg.fileserverHits.Load())
 	w.Write([]byte(response))
 }
 
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request) {
+	if os.Getenv("PLATFORM") != "dev" {
+		w.WriteHeader(403)
+		return
+	}
 	cfg.fileserverHits.Store(0)
+	cfg.db.DeleteAllUsers(req.Context())
 	w.WriteHeader(200)
+}
+
+func (cfg *apiConfig) createUserHandler(w http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+
+	type User struct {
+		ID        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Email     string    `json:"email"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 500, "Error decoding parameters", err)
+		return
+	}
+
+	user, err := cfg.db.CreateUser(
+		req.Context(),
+		sql.NullString{String: params.Email, Valid: true},
+	)
+	if err != nil {
+		respondWithError(w, 500, "Not able to creare User", err)
+		return
+	}
+	respondWithJSON(w, 201, User{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt.Time,
+		UpdatedAt: user.UpdatedAt.Time,
+		Email:     user.Email.String,
+	})
 }
 
 func healthzHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+func validateChirpHandler(w http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	type returnVal struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 500, "Error decoding parameters", err)
+		return
+	}
+
+	// The body can't be over 140 characters
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long", nil)
+		return
+	}
+
+	cleanBody := cleanResponse(params.Body)
+
+	respondWithJSON(w, 200, returnVal{CleanedBody: cleanBody})
+}
+
+func cleanResponse(body string) string {
+	var cleanBody []string
+	listBadWords := []string{"kerfuffle", "sharbert", "fornax"}
+	for word := range strings.SplitSeq(body, " ") {
+		if slices.Contains(listBadWords, strings.ToLower(word)) {
+			cleanBody = append(cleanBody, "****")
+		} else {
+			cleanBody = append(cleanBody, word)
+		}
+	}
+	return strings.Join(cleanBody, " ")
 }
